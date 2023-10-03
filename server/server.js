a5a3dd5faeaa15c58b3a4d94a9a7c01e58e5b9b5
Nhat Tan <nhattan8421@gmail.com>
2023-10-03 21:46:27 +0700

    update server

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
-const express = require('express')
-require('dotenv').config()
+const express = require("express");
+require("dotenv").config();
+const dbConnect = require("./config/dbConnect");
+const initRoutes = require("./routes");
 
-const app = express()
-const port = process.env.PORT || 8888
-app.use(express.json())
-app.use(express.urlencoded({ extended : true }))
+const app = express();
+const port = process.env.PORT || 8888;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+dbConnect();
 
-app.use('/', (req, res) => { res.send('SERVER ON') })
+initRoutes(app);
 
 app.listen(port, () => {
-    console.log('Server running on port: ' +port);
-})
+  console.log("Server running on port: " + port);
+});
