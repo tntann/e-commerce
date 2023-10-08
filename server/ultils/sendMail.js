5c49bafda4e4afbb77b1b22dacca7a66e4b87d76
Nhat Tan <nhattan8421@gmail.com>
2023-10-08 23:59:34 +0700

    add sendMail

---
+++ server/ultils/sendMail.js
@@ -0,0 +1,27 @@
+const nodemailer = require("nodemailer");
+const asyncHandler = require("express-async-handler");
+
+const sendMail = asyncHandler(async ({ email, html }) => {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.EMAIL_NAME,
+      pass: process.env.EMAIL_APP_PASSWORD,
+    },
+  });
+
+  // send mail with defined transport object
+  const info = await transporter.sendMail({
+    from: '"He E-commerce 👻" <no-reply@heshop.com>', // sender address
+    to: email, // list of receivers
+    subject: "Forgot password", // Subject line
+    // text: "Hello world?", // plain text body
+    html: html, // html body
+  });
+
+  return info;
+});
+
+module.exports = sendMail;
