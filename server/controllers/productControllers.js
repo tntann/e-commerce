25aec0dccd4b3d85edbeaf8f782eaa212a49563c
Nhat Tan <nhattan8421@gmail.com>
2023-10-09 00:01:04 +0700

    add product Controllers,Models,Router

---
+++ server/controllers/productControllers.js
@@ -0,0 +1,64 @@
+const Product = require("../models/productModels");
+const asyncHandler = require("express-async-handler");
+const slugify = require("slugify");
+
+// tao san pham
+const createProduct = asyncHandler(async (req, res) => {
+  if (Object.keys(req.body).length === 0) throw new Error("Missing inputs");
+  if (req.body && req.body.title) req.body.slug = slugify(req.body.title);
+  const newProduct = await Product.create(req.body);
+  return res.status(200).json({
+    success: newProduct ? true : false,
+    createdProduct: newProduct ? newProduct : "Cannot create new product",
+  });
+});
+
+// get product
+const getProduct = asyncHandler(async (req, res) => {
+  const { pid } = req.params;
+  const product = await Product.findById(pid);
+  return res.status(200).json({
+    success: product ? true : false,
+    productData: product ? product : "Cannot get product",
+  });
+});
+
+// get nhieu products
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find();
+  return res.status(200).json({
+    success: products ? true : false,
+    productDatas: products ? products : "Cannot get products",
+  });
+});
+
+// update product
+const updateProduct = asyncHandler(async (req, res) => {
+  const { pid } = req.params;
+  if (req.body && req.body.title) req.body.slug = slugify(req.body.title);
+  const updatedProduct = await Product.findByIdAndUpdate(pid, req.body, {
+    new: true,
+  });
+  return res.status(200).json({
+    success: updatedProduct ? true : false,
+    updatedProduct: updatedProduct ? updatedProduct : "Cannot update product",
+  });
+});
+
+// delete product
+const deleteProduct = asyncHandler(async (req, res) => {
+  const { pid } = req.params;
+  const deletedProduct = await Product.findByIdAndDelete(pid);
+  return res.status(200).json({
+    success: deletedProduct ? true : false,
+    deletedProduct: deletedProduct ? deletedProduct : "Cannot delete product",
+  });
+});
+
+module.exports = {
+  createProduct,
+  getProduct,
+  getProducts,
+  updateProduct,
+  deleteProduct,
+};
