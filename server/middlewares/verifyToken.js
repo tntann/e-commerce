dc0ec3486e5a31f022ae924b3af31937d6456691
Nhat Tan <nhattan8421@gmail.com>
2023-10-09 00:00:00 +0700

    add verifyToken

---
+++ server/middlewares/verifyToken.js
@@ -0,0 +1,40 @@
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+
+const verifyAccessToken = asyncHandler(async (req, res, next) => {
+  // Bearer token
+  // headers: {authorization: Bearer Token}
+  if (req?.headers?.authorization?.startsWith("Bearer")) {
+    const token = req.headers.authorization.split(" ")[1];
+    jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
+      if (err)
+        return res.status(401).json({
+          success: false,
+          mess: "Invalid access token",
+        });
+      //   console.log(decode);
+      req.user = decode;
+      next();
+    });
+  } else {
+    return res.status(401).json({
+      success: false,
+      mess: "Require authentication!",
+    });
+  }
+});
+
+const isAdmin = asyncHandler((req, res, next) => {
+  const { role } = req.user;
+  if (role !== "admin")
+    return res.status(401).json({
+      success: false,
+      mess: "Require Admin Role",
+    });
+  next();
+});
+
+module.exports = {
+  verifyAccessToken,
+  isAdmin,
+};
