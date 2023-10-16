74692eb39ca5eb2355045c73283fbb169764e021
Nhat Tan <nhattan8421@gmail.com>
2023-10-16 16:17:48 +0700

    add productCategoryRouter

---
+++ server/routes/productCategoryRouter.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+const ctrls = require("../controllers/prodCategoryControllers");
+const { verifyAccessToken, isAdmin } = require("../middlewares/verifyToken");
+
+router.post("/", [verifyAccessToken, isAdmin], ctrls.createCategory);
+router.get("/", ctrls.getCategories);
+
+router.put("/:pcid", [verifyAccessToken, isAdmin], ctrls.updateCategory);
+router.delete("/:pcid", [verifyAccessToken, isAdmin], ctrls.deleteCategory);
+
+module.exports = router;
