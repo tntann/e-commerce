e37c8e9dfb575518c2ae0fc06a0c729d04c6c014
Nhat Tan <nhattan8421@gmail.com>
2023-10-16 16:16:02 +0700

    add blogModels

---
+++ server/models/blogModels.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose"); // Erase if already required
+
+// Declare the Schema of the Mongo model
+var blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    numberViews: {
+      type: Number,
+      default: 0,
+    },
+    likes: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    dislikes: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    images: {
+      type: String,
+      default:
+        "https://png.pngtree.com/thumb_back/fh260/background/20201010/pngtree-black-friday-sell-background-design-image_408821.jpg",
+    },
+    author: {
+      type: String,
+      default: "Admin",
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+//Export the model
+module.exports = mongoose.model("Blog", blogSchema);
