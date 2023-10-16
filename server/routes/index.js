05a25dc5d8ceef63eef4aef5d3b0949b481b8a39
Nhat Tan <nhattan8421@gmail.com>
2023-10-16 16:14:01 +0700

    update index

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,19 @@
 const userRouter = require("./userRouter");
 const productRouter = require("./productRouter");
+const productCategoryRouter = require("./productCategoryRouter");
+const blogCategoryRouter = require("./blogCategoryRouter");
+const brandRouter = require("./brandRouter");
+const blogRouter = require("./blogRouter");
+
 const { notFound, errHandler } = require("../middlewares/errHandler");
 
 const initRoutes = (app) => {
   app.use("/api/user", userRouter);
   app.use("/api/product", productRouter);
+  app.use("/api/prodcategory", productCategoryRouter);
+  app.use("/api/blogcategory", blogCategoryRouter);
+  app.use("/api/blog", blogRouter);
+  app.use("/api/brand", brandRouter);
 
   app.use(notFound);
   app.use(errHandler);
