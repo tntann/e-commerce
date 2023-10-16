87e456f661b974895af4f694d1b55fd763475227
Nhat Tan <nhattan8421@gmail.com>
2023-10-16 16:15:06 +0700

    add blogCategoryControllers

---
+++ server/controllers/blogCategoryControllers.js
@@ -0,0 +1,48 @@
+const BlogCategory = require("../models/blogCategory");
+const asyncHandler = require("express-async-handler");
+
+const createCategory = asyncHandler(async (req, res) => {
+  const response = await BlogCategory.create(req.body);
+  return res.status(200).json({
+    success: response ? true : false,
+    createdCategory: response ? response : "Cannot create new blog category",
+  });
+});
+
+// get categories
+const getCategories = asyncHandler(async (req, res) => {
+  const response = await BlogCategory.find().select("title _id");
+  return res.status(200).json({
+    success: response ? true : false,
+    blogCategories: response ? response : "Cannot get blog category",
+  });
+});
+
+// update blog category
+const updateCategory = asyncHandler(async (req, res) => {
+  const { bcid } = req.params;
+  const response = await BlogCategory.findByIdAndUpdate(bcid, req.body, {
+    new: true,
+  });
+  return res.status(200).json({
+    success: response ? true : false,
+    updatedCategory: response ? response : "Cannot update blog category",
+  });
+});
+
+// delete blog category
+const deleteCategory = asyncHandler(async (req, res) => {
+  const { bcid } = req.params;
+  const response = await BlogCategory.findByIdAndDelete(bcid);
+  return res.status(200).json({
+    success: response ? true : false,
+    deletedCategory: response ? response : "Cannot delete blog category",
+  });
+});
+
+module.exports = {
+  createCategory,
+  getCategories,
+  updateCategory,
+  deleteCategory,
+};
