b46a2420418737d00242d1ce566a8835eadabacf
Nhat Tan <nhattan8421@gmail.com>
2023-10-16 16:15:24 +0700

    add blogControllers

---
+++ server/controllers/blogControllers.js
@@ -0,0 +1,152 @@
+const Blog = require("../models/blogModels");
+const asyncHandler = require("express-async-handler");
+
+// Create a new blog
+const createNewBlog = asyncHandler(async (req, res) => {
+  const { title, description, category } = req.body;
+  if (!title || !description || !category) throw new Error("Missing inputs");
+  const response = await Blog.create(req.body);
+  return res.status(200).json({
+    success: response ? true : false,
+    createdBlog: response ? response : "Cannot create new blog",
+  });
+});
+
+// get blogs
+const getBlogs = asyncHandler(async (req, res) => {
+  const response = await Blog.find();
+  return res.status(200).json({
+    success: response ? true : false,
+    blogs: response ? response : "Cannot get blogs",
+  });
+});
+
+// update blog
+const updateBlog = asyncHandler(async (req, res) => {
+  const { bid } = req.params;
+  if (Object.keys(req.body).length === 0) throw new Error("Missing inputs");
+  const response = await Blog.findByIdAndUpdate(bid, req.body, { new: true });
+  return res.status(200).json({
+    success: response ? true : false,
+    updatedBlog: response ? response : "Cannot update blog",
+  });
+});
+
+// delete blog
+const deleteBlog = asyncHandler(async (req, res) => {
+  const { bid } = req.params;
+  const blog = await Blog.findByIdAndDelete(bid);
+  return res.status(200).json({
+    success: blog ? true : false,
+    deleteBlog: blog ? blog : "Cannot delete blog",
+  });
+});
+
+// like
+const likeBlog = asyncHandler(async (req, res) => {
+  const { _id } = req.user;
+  const { bid } = req.params;
+  if (!bid) throw new Error("Missing inputs");
+  const blog = await Blog.findById(bid);
+  const alreadyDisliked = blog?.dislikes?.find((el) => el.toString() === _id);
+  if (alreadyDisliked) {
+    const response = await Blog.findByIdAndUpdate(
+      bid,
+      { $pull: { dislikes: _id } },
+      { new: true }
+    );
+    return res.status(200).json({
+      success: response ? true : false,
+      result: response,
+    });
+  }
+  const isLiked = blog?.likes?.find((el) => el.toString() === _id);
+  if (isLiked) {
+    const response = await Blog.findByIdAndUpdate(
+      bid,
+      { $pull: { likes: _id } },
+      { new: true }
+    );
+    return res.status(200).json({
+      success: response ? true : false,
+      result: response,
+    });
+  } else {
+    const response = await Blog.findByIdAndUpdate(
+      bid,
+      { $push: { likes: _id } },
+      { new: true }
+    );
+    return res.status(200).json({
+      success: response ? true : false,
+      result: response,
+    });
+  }
+});
+
+// dislike
+const dislikeBlog = asyncHandler(async (req, res) => {
+  const { _id } = req.user;
+  const { bid } = req.params;
+  if (!bid) throw new Error("Missing inputs");
+  const blog = await Blog.findById(bid);
+  const alreadyLiked = blog?.likes?.find((el) => el.toString() === _id);
+  if (alreadyLiked) {
+    const response = await Blog.findByIdAndUpdate(
+      bid,
+      { $pull: { likes: _id } },
+      { new: true }
+    );
+    return res.status(200).json({
+      success: response ? true : false,
+      result: response,
+    });
+  }
+  const isDisliked = blog?.dislikes?.find((el) => el.toString() === _id);
+  if (isDisliked) {
+    const response = await Blog.findByIdAndUpdate(
+      bid,
+      { $pull: { dislikes: _id } },
+      { new: true }
+    );
+    return res.status(200).json({
+      success: response ? true : false,
+      result: response,
+    });
+  } else {
+    const response = await Blog.findByIdAndUpdate(
+      bid,
+      { $push: { dislikes: _id } },
+      { new: true }
+    );
+    return res.status(200).json({
+      success: response ? true : false,
+      result: response,
+    });
+  }
+});
+
+const getBlog = asyncHandler(async (req, res) => {
+  const { bid } = req.params;
+  const blog = await Blog.findByIdAndUpdate(
+    bid,
+    { $inc: { numberViews: 1 } },
+    { new: true }
+  )
+    .populate("likes", "firstname lastname")
+    .populate("dislikes", "firstname lastname");
+  return res.status(200).json({
+    success: blog ? true : false,
+    result: blog,
+  });
+});
+
+module.exports = {
+  createNewBlog,
+  updateBlog,
+  deleteBlog,
+  getBlogs,
+  likeBlog,
+  dislikeBlog,
+  getBlog,
+};
