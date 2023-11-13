4f74bb88fce9674ac9476509eafd577ea50cad24
Nhat Tan <nhattan8421@gmail.com>
2023-11-13 16:56:34 +0700

    update data

--- data/cate_brand.js
+++ data/cate_brand.js
@@ -12,50 +12,112 @@ const data = [
       "Vivo",
     ],
     icon: "https://res.cloudinary.com/dxicjtlt4/image/upload/v1698496247/ecommerce/smartphone.webp",
+    image:
+      "https://res.cloudinary.com/dxicjtlt4/image/upload/v1698589193/ecommerce/phone-img_wag0md.webp",
   },
   {
     cate: "Tablet",
-    brand: ["Apple", "Samsung", "Acer", "Asus", "Lenovo", "Haier", "Xiaomi"],
+    brand: [
+      "Apple",
+      "Samsung",
+      "Acer",
+      "Asus",
+      "Lenovo",
+      "TCL",
+      "Xiaomi",
+      "Huawei",
+    ],
     icon: "https://res.cloudinary.com/dxicjtlt4/image/upload/v1698496247/ecommerce/tablet.webp",
+    image:
+      "https://res.cloudinary.com/dxicjtlt4/image/upload/v1698589192/ecommerce/tablet-img_f2hke5.webp",
   },
   {
     cate: "Laptop",
-    brand: ["Asus", "Dell", "Acer", "Lenovo", "HP", "Macbook", "MSI"],
+    brand: [
+      "Asus",
+      "Dell",
+      "Acer",
+      "Lenovo",
+      "HP",
+      "Macbook",
+      "MSI",
+      "Gigabyte",
+    ],
     icon: " https://res.cloudinary.com/dxicjtlt4/image/upload/v1698496247/ecommerce/laptop.png",
+    image:
+      "https://res.cloudinary.com/dxicjtlt4/image/upload/v1698589192/ecommerce/mac-img_ldfiaq.webp",
   },
   {
     cate: "Accessories",
     brand: [
       "Smartwatches",
       "Headphone",
-      "Bluetooth",
+      "Caple",
       "Keyboard",
+      "Battery backup",
+      "Webcam",
       "Mouse",
       "Cases",
     ],
     icon: "https://res.cloudinary.com/dxicjtlt4/image/upload/v1698496247/ecommerce/accessories.webp",
+    image:
+      "https://res.cloudinary.com/dxicjtlt4/image/upload/v1698589191/ecommerce/phukien_t2nukq.webp",
   },
   {
     cate: "Speaker",
-    brand: ["Asus", "Dell", "Acer", "Lenovo", "HP", "Samsung", "LG"],
+    brand: [
+      "Marshall",
+      "Apple",
+      "Xiaomi",
+      "Edifier",
+      "JBL",
+      "Samsung",
+      "LG",
+      "Sony",
+    ],
     icon: "https://res.cloudinary.com/dxicjtlt4/image/upload/v1698496247/ecommerce/speaker.webp",
+    image:
+      "https://res.cloudinary.com/dxicjtlt4/image/upload/v1698593610/ecommerce/loa_ww3wz8.webp",
   },
 
   {
     cate: "Camera",
-    brand: ["Asus", "Dell", "Acer", "Lenovo", "HP", "Samsung", "LG"],
+    brand: [
+      "Security Camera",
+      "Dashcam",
+      "Action Camera",
+      "Gimbal",
+      "Tripod",
+      "Camera",
+      "Drones",
+    ],
     icon: "https://res.cloudinary.com/dxicjtlt4/image/upload/v1698496247/ecommerce/camera.webp",
+    image:
+      "https://res.cloudinary.com/dxicjtlt4/image/upload/v1698589527/ecommerce/camera_z8qctz.webp",
   },
 
   {
     cate: "Televisions",
-    brand: ["Apple", "Samsung", "LG", "Asus", "Nokia", "BlackBerry"],
+    brand: [
+      "TCL",
+      "Samsung",
+      "LG",
+      "Toshiba",
+      "Casper",
+      "Coocaa",
+      "Xiaomi",
+      "Sony",
+    ],
     icon: "https://res.cloudinary.com/dxicjtlt4/image/upload/v1698496247/ecommerce/television.png",
+    image:
+      "https://res.cloudinary.com/dxicjtlt4/image/upload/v1698589192/ecommerce/tv-img_ws1urp.webp",
   },
   {
     cate: "Printer",
-    brand: ["Samsung", "Acer", "Asus", "Lenovo", "Haier"],
+    brand: ["Canon", "Brother", "HP", "Espon", "Lenovo", "Laser", "LG"],
     icon: "https://res.cloudinary.com/dxicjtlt4/image/upload/v1698496247/ecommerce/printer.webp",
+    image:
+      "https://res.cloudinary.com/dxicjtlt4/image/upload/v1698589191/ecommerce/printer-ing_otqlhj.webp",
   },
 ];
 
